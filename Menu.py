0c7bbd9ba1b876d0ee694de40ab69a6fdf9a2b18
tesskoo <tkoo@chadwickschool.org>
2016-10-25 12:05:03 -0700

    finish lists and random choices

--- Menu.py
+++ Menu.py
@@ -1,20 +1,34 @@
 from Myro import *
 
 #Chinese food lists
-list1 = ['Spicy','Broiled','Grilled']
+list1 = ['Fried','Broiled','Grilled','Honey']
 list2 = ['Chicken','Beef','Shrimp','Lamb']
 list3 = ['with sweet and sour sauce','hot sauce','soy sauce','sesame sauce']
 #American food lists
+list4 = ['Chicken Fried','Grilled','Breaded','Baked']
+list5 = ['Steak','Chicken','Pork','Bison']
+list6 = ['mashed potatoes','french frieds','cole slaw','creamed spinach']
 
 #Italian food lists
+list7 = ['Alfredo','Pesto','Marinara','Bolognese']
+list8 = ['linquini','penne','rigatoni','angel hair pasta','fusili','spaghetti']
+list9 = ['meatballs','filet mignon','prosciutto','basil','oregano']
 
+import random
 
 answer=askQuestion("What kind of food would you like?", ["Chinese", "American", "Italian"])
 if (answer=="Chinese"):
     print("Tess's Authentic Chinese food")
-    print("%s %s with %s") % (list1[1], list2[1], list3[1])
+    print("Entrees")
+    for x in range(0,5):
+        print(random.choice(list1), random.choice(list2),"with", random.choice(list3))
 if (answer=="American"):
-    print("Tess's Bistro")
+    print("Tess's Grill")
+    print("Entrees")
+    for x in range(0,5):
+        print(random.choice(list4), random.choice(list5),"with", random.choice(list6))
 if (answer=="Italian"):
     print("Mama Koo's")
-
+    print("Entrees")
+    for x in range(0,5):
+        print(random.choice(list7), random.choice(list8),"with", random.choice(list9))
