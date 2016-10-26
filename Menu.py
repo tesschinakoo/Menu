c78d096c91dbc8e4f66343f4a099e20f410e9ff6
tesskoo <tkoo@chadwickschool.org>
2016-10-26 14:39:03 -0700

    add GUI and make it so menu items don't repeat

--- Menu.py
+++ Menu.py
@@ -2,51 +2,62 @@ from Myro import *
 
 #Chinese food lists
 list1 = ['Fried','Broiled','Grilled','Stir-fry']
-list2 = ['Chicken','Beef','Shrimp','Lamb']
-list3 = ['sweet and sour sauce','hot sauce','sweet soy sauce','sesame sauce']
+list2 = [' Chicken',' Beef',' Shrimp',' Lamb']
+list3 = [' sweet and sour sauce',' hot sauce',' sweet soy sauce',' sesame sauce']
 #American food lists
 list4 = ['Chicken Fried','Grilled','Breaded','Baked']
-list5 = ['Steak','Chicken','Pork','Bison']
-list6 = ['mashed potatoes','french frieds','cole slaw','creamed spinach']
+list5 = [' Steak',' Chicken',' Pork',' Bison']
+list6 = [' mashed potatoes',' french fries',' cole slaw',' creamed spinach']
 
 #Italian food lists
 list7 = ['Alfredo','Pesto','Marinara','Bolognese','Parmasean','Butter']
-list8 = ['linquini','penne','rigatoni','angel hair pasta','fusili','spaghetti']
-list9 = ['meatballs','filet mignon','prosciutto','basil','oregano','sausage']
+list8 = [' linquini',' penne',' rigatoni',' angel hair pasta',' fusili',' spaghetti']
+list9 = [' meatballs',' filet mignon',' prosciutto',' basil',' oregano',' sausage']
 
 import random
 
+from Graphics import *
+
+win = Window("Menu")
+
+
 answer=askQuestion("What kind of food would you like?", ["Chinese", "American", "Italian"])
 if (answer=="Chinese"):
-    print("Tess's Authentic Chinese food")
-    print("Entrees")
-    for x in range(0,4):
-        choice1 = random.choice(list1)
-        choice2 = random.choice(list2)
-        choice3 = random.choice(list3)
-        print(choice1, choice2,"with", choice3)
-        list1.remove(choice1)
-        list2.remove(choice2)
-        list3.remove(choice3)
+    Restaurant = "Tess's Authentic Chinese Food"
+    Type = list1
+    Entree = list2
+    Side = list3
 if (answer=="American"):
-    print("Tess's Grill")
-    print("Entrees")
-    for x in range(0,4):
-        choice4 = random.choice(list4)
-        choice5 = random.choice(list5)
-        choice6 = random.choice(list6)
-        print(choice4, choice5,"with", choice6)
-        list4.remove(choice4)
-        list5.remove(choice5)
-        list6.remove(choice6)
+    Restaurant = "Tess's Grill"
+    Type = list4
+    Entree = list5
+    Side = list6
 if (answer=="Italian"):
-    print("Mama Koo's")
-    print("Entrees")
-    for x in range(0,6):
-        choice7 = random.choice(list7)
-        choice8 = random.choice(list8)
-        choice9 = random.choice(list9)
-        print(choice7, choice8,"with", choice9)
-        list7.remove(choice7)
-        list8.remove(choice8)
-        list9.remove(choice9)
+    Restaurant = "Mama Koo's"
+    Type = list7
+    Entree = list8
+    Side = list9
+    
+
+RestaurantName = Text((150, 30), Restaurant)
+RestaurantName.fontSize = 15
+RestaurantName.fill = Color("black")
+RestaurantName.draw(win)
+Entrees = Text((150,50), "Entrees")
+Entrees.fontSize = 14
+Entrees.fill = Color("black")
+Entrees.draw(win)
+for x in range(0,4):
+    choice1 = random.choice(Type)
+    choice2 = random.choice(Entree)
+    choice3 = random.choice(Side)
+    MenuItem1 = (choice1 + choice2 + " with" + choice3)
+    Entree1 = Text((150, 85 + (x* 20)), MenuItem1)
+    Entree1.fill = Color("black")
+    Entree1.fontSize = 13
+    Entree1.draw(win)
+    Type.remove(choice1)
+    Entree.remove(choice2)
+    Side.remove(choice3)
+
+
